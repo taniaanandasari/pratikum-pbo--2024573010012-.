e00354baf33ad81f605501b97614af1375ef799b
taniaanandasari <anandasaritania@gmail.com>
2025-09-11 16:37:18 +0700

    typedata

---
+++ src/tipedata/TipeData.java
@@ -0,0 +1,25 @@
+package tipedata;
+
+public class TipeData {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggibadan;
+        char jk;
+        float beratbadan;
+
+        nama = "Tania Ananda Sari";
+        alamat = "Batuphat Barat";
+        tinggibadan = 157;
+        usia = 20;
+        jk = "p";
+        beratbadan = "51.8f";
+
+        System.out.println("nama:" + nama);
+        System.out.println("alamat:" + alamat);
+        System.out.println("tinggibadan:" + tinggibadan);
+        System.out.println("usia:" + usia);
+        System.out.println("jk:" + jk);
+        System.out.println("beratbadan:" + beratbadan);
+    }
+
